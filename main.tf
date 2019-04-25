0f1bf199b61c37ced4f4845dcd52f4ab2bcc8b2e
Richard Hine <rhine59@gmail.com>
2019-04-25 10:26:26 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -44,26 +44,24 @@ echo 10.134.214.138  utility >> /etc/hosts
 apt install -y unzip
 echo "*INFO* unzip install RC $?"
 echo "*INFO* downloading Websphere Liberty ....."
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - downloading Websphere Liberty"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Downloading Websphere Liberty"}' \
 	https://hooks.slack.com/services/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
 cd /tmp && wget http://utility/export/Liberty/daytrader8Server.zip > /dev/null 2>&1
 echo "*INFO* downloaded Websphere Liberty RC $?"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Webspehere Liberty downloaded RC $?"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Websphere Liberty downloaded"}' \
 	https://hooks.slack.com/services/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
 echo "*INFO* extracted Websphere Liberty ....."
 cd / && unzip /tmp/daytrader8Server.zip > /dev/null 2>&1
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Webspehere Liberty installed RC $?"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Websphere Liberty installed"}' \
 	https://hooks.slack.com/services/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
 echo "*INFO* extracted Websphere Liberty RC $?"
 echo "*INFO* starting Websphere Liberty ....."
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - starting Websphere Liberty with daytrader8 application"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Starting Websphere Liberty with daytrader8 application"}' \
 	https://hooks.slack.com/services/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
 /wlp/bin/server start daytrader8Server 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Websphere Liberty and daytrader8 started RC $?"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Websphere Liberty and daytrader8 started"}' \
 	https://hooks.slack.com/services/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
 echo "*INFO* Websphere Liberty started RC $?"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"SUCCESS - Please use http://10.134.214.161:9080/daytrader/ to connect to your new workload"}' \
-	https://hooks.slack.com/services/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
 echo "*INFO* execution of /tmp/install_software.sh complete"
 exit 0
 EOF
@@ -89,11 +87,15 @@ resource "null_resource" "config-static-route-fiweb" {
     content = <<EOF
 #!/bin/bash -x
 echo "--- customise fiweb begin ---"
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Starting network configuration"}' \
+	https://hooks.slack.com/services/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
 cp /etc/network/interfaces /etc/network/interfaces.tmp
 grep -iv route /etc/network/interfaces.tmp > /etc/network/interfaces
 echo up route add -net ${var.network_route1} netmask ${var.network_netmask1} gw ${var.network_gateway1} >> /etc/network/interfaces
 echo up route add -net ${var.network_route2} netmask ${var.network_netmask2} gw ${var.network_gateway2} >> /etc/network/interfaces
 systemctl restart networking
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fiweb - Network configuration complete"}' \
+	https://hooks.slack.com/services/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
 echo "--- customise fiweb end ---"
 exit 0
 EOF
@@ -162,7 +164,7 @@ resource "null_resource" "config-static-route-fidb" {
     content = <<EOF
 #!/bin/bash -x
 echo "--- customise fidb begin ---"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - starting network configuration"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - Starting network configuration"}' \
 	https://hooks.slack.com/services/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
 echo 10.134.214.138  utility >> /etc/hosts
 cp /etc/network/interfaces /etc/network/interfaces.tmp
@@ -170,7 +172,7 @@ grep -iv route /etc/network/interfaces.tmp > /etc/network/interfaces
 echo up route add -net ${var.network_route1} netmask ${var.network_netmask1} gw ${var.network_gateway1} >> /etc/network/interfaces
 echo up route add -net ${var.network_route2} netmask ${var.network_netmask2} gw ${var.network_gateway2} >> /etc/network/interfaces
 systemctl restart networking
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - network configuration completed"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - Network configuration completed"}' \
 	https://hooks.slack.com/services/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
 echo "--- customise fidb end ---"
 exit 0
@@ -200,7 +202,7 @@ echo "*INFO* executing /tmp/install_software.sh"
 echo 10.134.214.138  utility >> /etc/hosts
 echo "*INFO* installing extra packages ....."
 apt install -y binutils apt-file lib32ncurses5 lib32z1 libaio1 lib32stdc++6 libpam0g:i386 > /dev/null 2>&1
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - extra packages installed RC $?"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - Extra OS packages installed"}' \
 	https://hooks.slack.com/services/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
 echo "*INFO* install complete RC $?"
 echo "*INFO* downloading DB2 ......"
@@ -213,10 +215,10 @@ echo "*INFO* downloading DB2 install response file"
 wget http://utility/export/DB2/db2server.rsp > /dev/null 2>&1
 echo "*INFO* downloading of DB2 install response file complete RC $?"
 echo "*INFO* installing DB2 ..............."
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - installing DB2 ......."}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - Installing DB2 ......."}' \
 	https://hooks.slack.com/services/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
 /tmp/server_dec/db2setup -r /tmp/db2server.rsp > /dev/null 2>&1
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - DB2 install completed RC $?"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - DB2 install completed"}' \
 	https://hooks.slack.com/services/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
 echo "*INFO* DB2 install completed RC $?"
 rm -fr /tmp/v11.1_linuxx64_dec.tar /tmp/server_dec
@@ -226,11 +228,13 @@ echo "*INFO* Restoring backup of TradeDB database"
 curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - restoring TRADEDB database backup ......."}' \
 	https://hooks.slack.com/services/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
 su - db2inst1 -c 'db2 restore db tradedb replace existing redirect; db2 restore db tradedb continue; db2 activate db tradedb'
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - DB2 TRADEDB database restore completed RC $?"}' \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - DB2 TRADEDB database restore completed"}' \
 	https://hooks.slack.com/services/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
 echo "*INFO* restore replace of TradeDB database completed RC $?"
 echo "*INFO* execution of /tmp/install_software.sh completed"
 curl -X POST -H 'Content-type: application/json' --data '{"text":"fidb - configuration complete"}' \
+	https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"SUCCESS - Please use http://10.134.214.161:9080/daytrader/ to connect to your new workload"}' \
 	https://hooks.slack.com/services/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
 exit 0
 EOF
